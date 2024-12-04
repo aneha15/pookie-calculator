924d9f2073c2dc7c09ea5c85b4eba2a7700c31db
neha1562005 <76821812+neha1562005@users.noreply.github.com>
2024-12-04 15:40:38 +0600

    Make buttons functional

--- script.js
+++ script.js
@@ -1,19 +1,54 @@
-function add(a, b) {
+add = function addition(a, b) {
     return a + b;
-}
+};
 
-function subtract(a, b) {
+subract = function subtraction(a, b) {
     return a - b;
-}
+};
 
-function multiply(a, b) {
+multiply = function multiplication(a, b) {
     return a * b;
 }
 
-function divide(a, b) {
+divide = function division(a, b) {
     return a / b;
-}
+};
 
-function oneOp(x, operator, y) {
+function operate(x, operator, y) {
+    if (operator == "+") {
+        add(x, y);
+    } else if (operator == "−") {
+        subtract(x, y);
+    } else if (operator == "x") {
+        multiply(x, y);
+    } else if (operator == "÷") {
+        divide(x, y);
+    } else {
+        return 0;  // fix for % later
+    }
+
+};
+
+function populateDisplay() {
     
-}
+};
+
+const input = document.querySelector(".input");
+const numberBtns = document.querySelectorAll(".number")
+const operatorBtns = document.querySelectorAll(".operator");
+const equalBtn = document.querySelector(".equal");
+const clearBtn = document.querySelector(".clear");
+const deleteBtn = document.querySelector(".delete");
+
+numberBtns.forEach(button => {
+    button.addEventListener("click", () => {
+        let text = button.innerText;
+        populateDisplay();
+    })
+})
+operatorBtns.forEach(button => {
+    button.addEventListener("click", () => {
+        let text = button.innerText;
+        populateDisplay();
+    })
+})
